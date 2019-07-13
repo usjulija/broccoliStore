7ac833da8ce57834923668559ce1e64158b7ebce
Julia <us.julija@gmail.com>
2019-07-13 14:47:09 +0300

    blank application

---
+++ src/components/BroccoliStore.js
@@ -0,0 +1,11 @@
+import React from 'react';import { format } from 'path';
+
+class BroccoliStore extends React.Component {
+    render() {
+        return (
+            <p>Hello</p>
+        );
+    }
+}
+
+export default BroccoliStore;
