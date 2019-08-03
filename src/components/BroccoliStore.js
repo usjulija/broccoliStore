372fd9daf0c0028c4dbff5949e88ce753b20b7f9
Julia <us.julija@gmail.com>
2019-08-03 19:18:54 +0300

    navigation menu added

--- src/components/BroccoliStore.js
+++ src/components/BroccoliStore.js
@@ -1,9 +1,26 @@
-import React from 'react';import { format } from 'path';
+import React from 'react';
+import StoreMenu from './StoreMenu';
 
 class BroccoliStore extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          mobileVisible: false //state of mobile menu
+        };
+        this.toggleNavMenu = this.toggleNavMenu.bind(this);
+      }
+    
+      //makes mobile menu visible & invisible
+      toggleNavMenu() {
+        this.setState({
+            mobileVisible: !this.state.mobileVisible
+        });
+      }
     render() {
         return (
-            <p>Hello</p>
+            <StoreMenu
+                toggleNavMenu={this.toggleNavMenu}
+                mobileVisible={this.state.mobileVisible} />
         );
     }
 }
